0bd69aedfbb2e4d6d112f9686fb978344fd9ddf4
ANUPAM GUPTA <anupamg2001@gmail.com>
2025-09-21 10:23:20 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/dash_rise/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dash_rise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/game_cubit/game_cubit.dart';
+import 'home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(
+    DevicePreview(
+      enabled: false,
+      tools: const [...DevicePreview.defaultTools],
+      builder: (context) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => GameCubit(),
+      child: MaterialApp(
+        theme: ThemeData(fontFamily: 'Chewy'),
+        debugShowCheckedModeBanner: false,
+        title: 'DashRise',
+        home: const HomePage(),
+      ),
+    );
+  }
+}
